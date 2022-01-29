dd7542e013d495ce3ecc75085f2d248c80876102
fefta <stuffefta@gmail.com>
2022-01-29 12:52:47 +0300

    models, main page, db, initial in another words

--- figures/models.py
+++ figures/models.py
@@ -11,15 +11,23 @@ class Client(models.Model):
 
 
 class Circle(models.Model):
-	CIRCLE_TYPES = (
+	FTYPES = (
 		('O', 'Ordinary'),
 		('R', 'Rare'),
 		('U', 'Unique'),
 	)
 	name = models.CharField(max_length=30, unique=True)
-	circle_type = models.CharField(max_length=1, choices=CIRCLE_TYPES)
+	ftype = models.CharField(max_length=1, choices=FTYPES)
 	owner = models.ForeignKey(Client, on_delete=models.DO_NOTHING, null=True)
 	sending_date = models.DateTimeField(null=True)
 
 	def __str__(self):
 		return f'circle {self.name}'
+
+	@classmethod # TODO: move that classmethod to the parent class Figure when it created	
+	def not_sended_figures_by_types(cls):
+		ordinary_figures = cls.objects.filter(ftype='O', owner=None).count()
+		rare_figures = cls.objects.filter(ftype='R', owner=None).count()
+		unique_figures = cls.objects.filter(ftype='U', owner=None).count()
+
+		return ordinary_figures, rare_figures, unique_figures
--- figures/views.py
+++ figures/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Client, Circle
 
 # Create your views here.
+
+def description(request):
+	if request.method == 'GET':
+		ordinary_circles, rare_circles, unique_circles = Circle.not_sended_figures_by_types()
+		all_circles = ordinary_circles + rare_circles + unique_circles
+
+		return render(request, 'figures/main_page.html', {
+				'all_circles': all_circles,
+				'rare_circles': rare_circles,
+				'unique_circles': unique_circles
+			})
+
+	
